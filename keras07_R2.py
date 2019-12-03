a8cc4b6d70fdec59fd9a6f6ea7616d61733503bb
sangyup69 <ssangyup@naver.com>
2019-12-03 20:49:57 +0900

    1203_1

--- keras07_R2.py
+++ keras07_R2.py
@@ -14,28 +14,67 @@ model = Sequential()
 model.add(Dense(100, input_shape=(1, ), activation='relu')) 
 model.add(Dense(70))
 model.add(Dense(50))
-model.add(Dense(10))
+model.add(Dense(3))
 model.add(Dense(1))
 
 model.summary()
 
+# 선형회귀(y=wx+b), 분류 
+# 분류모델에서는 accuracy를 사용, 회귀모델에서는 mse를 사용
 # model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 model.fit(x_train,y_train, epochs=100, batch_size=1)
 
-loss, acc = model.evaluate(x_test, y_test, batch_size=1)
+loss, mse = model.evaluate(x_test, y_test, batch_size=1)
 # print('acc :', acc)
 print('mse :', mse)
 print('loss :', loss)
 
-'''
+
 y_predict = model.predict(x_test)
 print(y_predict)
 
-# rmse : mse에 root를 씌워서 loss값을 작게 한 평가지표
+# rmse 
 from  sklearn.metrics import mean_squared_error
 def RMSE(y_test, y_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))
 
 print("RMSE : ", RMSE(y_test, y_predict))
-'''
+
+# R2(1에 가까울수록 좋고, 0에 가까울수록 않좋다를 지표로만 사용)
+from  sklearn.metrics import r2_score
+r2_y_predict = r2_score(y_test, y_predict)
+print("R2 : ", r2_y_predict)
+
+
+# R2 값을 높이면, RMSE값은 낮아진다.
+ 
+#  mse : 0.8816581964492798
+# loss : 0.8816581562161445
+# [[10.576825]
+#  [11.473668]
+#  [12.369848]
+#  [13.265856]
+#  [14.161862]
+#  [15.05787 ]
+#  [15.953879]
+#  [16.849886]
+#  [17.745893]
+#  [18.641901]]
+# RMSE :  0.9389670778646177
+# R2 :  0.893132221416531
+
+# mse : 7.361268039574043e-09
+# loss : 7.361268217209727e-09
+# [[10.999893]
+#  [11.999898]
+#  [12.999903]
+#  [13.999908]
+#  [14.999913]
+#  [15.999917]
+#  [16.999922]
+#  [17.99993 ]
+#  [18.999931]
+#  [19.999939]]
+# RMSE :  8.580048347509828e-05
+# R2 :  0.9999999991076699
--- keras07_R2_test.py
+++ keras07_R2_test.py
@@ -10,32 +10,40 @@ y_test = np.array([11,12,13,14,15,16,17,18,19,20])
 
 
 model = Sequential()
-# model.add(Dense(100, input_dim=1, activation='relu'))    
-model.add(Dense(100, input_shape=(1, ), activation='relu')) 
-model.add(Dense(70))
-model.add(Dense(50))
-model.add(Dense(10))
+model.add(Dense(500, input_shape=(1, ), activation='relu'))
+model.add(Dense(5000))
+model.add(Dense(5000))
+model.add(Dense(5000))
+model.add(Dense(5000))
+model.add(Dense(5000))
 model.add(Dense(1))
 
 model.summary()
 
-# model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 model.fit(x_train,y_train, epochs=100, batch_size=1)
 
-loss, acc = model.evaluate(x_test, y_test, batch_size=1)
-# print('acc :', acc)
+loss, mse = model.evaluate(x_test, y_test, batch_size=1)
 print('mse :', mse)
 print('loss :', loss)
 
-'''
+
 y_predict = model.predict(x_test)
 print(y_predict)
 
-# rmse : mse에 root를 씌워서 loss값을 작게 한 평가지표
+# rmse 
 from  sklearn.metrics import mean_squared_error
 def RMSE(y_test, y_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))
 
 print("RMSE : ", RMSE(y_test, y_predict))
-'''
+
+# R2
+from  sklearn.metrics import r2_score
+r2_y_predict = r2_score(y_test, y_predict)
+print("R2 : ", r2_y_predict)
+ 
+# 문제1. R2를 0.5 이하로 줄이시오.(pc를 과부하시키면 정확도가 낮아짐)
+#  layer는 input과 output 포함 5개 이상, 노드는 각 layer당 5개 이상
+#  batch_size = 1
+#  epochs = 100 이상
