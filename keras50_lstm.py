7ae966309546383c414152d567c5685b6c68a665
sangyup69 <ssangyup@naver.com>
2019-12-24 16:01:12 +0900

    1224-1

--- keras50_lstm.py
+++ keras50_lstm.py
@@ -15,48 +15,38 @@ sheet = book.worksheets[0]
 xs = []
 ys = []
 for row in sheet.rows:
-    # data.append([
-    #     row[0].value,
-    #     row[1].value,
-    #     row[2].value,
-    #     row[3].value,
-    #     row[4].value
-    # ])
     ys.append(row[1].value)    
     xs.append([row[2].value, row[3].value, row[4].value])   
        
-x = array(xs)
-y = array(ys)
+x = array(ys)
+y = array(xs)
 
 # x = array([[1,2,3], [2,3,4], [3,4,5], [4,5,6]])
 # y = array([4,5,6,7])
-# print(x)
 print("x.shape : ", x.shape)
 print("y.shape : ", y.shape)
-# x    y
-# 123  4
-# 234  5
-# 345  6
-# 456  7
 
-x = x.reshape((x.shape[0], x.shape[1], 1))   # (4,3) -> (4,3,1)
+x = x.reshape((x.shape[0], 1, 1))   # (4,3) -> (4,3,1)
 print("x.shape : ", x.shape)   # (4, 3, 1)
 # print(x)
 
 #2. 모델구성
 model = Sequential()
-model.add(LSTM(300, activation='relu', input_shape=(3,1)))   # (행,3,1) input_shape에서 행은 무시
-model.add(Dense(100))                                         # 1 : 몇개씩 잘라서 작업할 것인가, 작업을 위한 cut size
+model.add(LSTM(30, activation='relu', input_shape=(1,1)))
+model.add(Dense(100))                                     
 model.add(Dense(50))
-model.add(Dense(1))
+model.add(Dense(3))
 model.summary()
 
 #3. 실행
+from keras.callbacks import EarlyStopping
+early_stopping = EarlyStopping(monitor='acc', patience=30, mode='auto')
 model.compile(optimizer='adam', loss='mse')
-model.fit(x, y, epochs=100)
+model.fit(x, y, epochs=100, batch_size=1, callbacks=[early_stopping])
 
-x_input = array([1.1086,1.294,109.34])
-x_input = x_input.reshape((1,3,1))
+x_input = array(1164.5)
+x_input = x_input.reshape((1,1,1))
 
 yhat = model.predict(x_input)
 print(yhat)
+
